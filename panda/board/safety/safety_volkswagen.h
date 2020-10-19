0933efed7acc9791cc87c0f971a38a6666d2518d
mike <mike@mikesantifort.nl>
2020-10-19 15:26:45 +0200

    Increase HCA heading code so it may increase steering performance.

--- panda/board/safety/safety_volkswagen.h
+++ panda/board/safety/safety_volkswagen.h
@@ -1,6 +1,6 @@
 // Safety-relevant steering constants for Volkswagen
 const int VOLKSWAGEN_MAX_STEER = 300;               // 3.0 Nm (EPS side max of 3.0Nm with fault if violated)
-const int VOLKSWAGEN_MAX_RT_DELTA = 75;             // 4 max rate up * 50Hz send rate * 250000 RT interval / 1000000 = 50 ; 50 * 1.5 for safety pad = 75
+const int VOLKSWAGEN_MAX_RT_DELTA = 188;             // 4 max rate up * 50Hz send rate * 250000 RT interval / 1000000 = 50 ; 50 * 1.5 for safety pad = 75
 const uint32_t VOLKSWAGEN_RT_INTERVAL = 250000;     // 250ms between real time checks
 const int VOLKSWAGEN_MAX_RATE_UP = 10;               // 2.0 Nm/s RoC limit (EPS rack has own soft-limit of 5.0 Nm/s)
 const int VOLKSWAGEN_MAX_RATE_DOWN = 10;            // 5.0 Nm/s RoC limit (EPS rack has own soft-limit of 5.0 Nm/s)
