33e58aba0579e4229a066aadd83fb929fe916420
mike <mike@mikesantifort.nl>
2020-10-30 19:18:58 +0100

    Upload over LTE

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -70,8 +70,8 @@ def is_on_wifi():
     # TODO: figure out why the android service call sometimes dies with SIGUSR2 (signal from MSGQ)
     result = android.parse_service_call_string(android.service_call(["connectivity", "2"]))
     if result is None:
-      return False
-    return False
+      return True
+    return True
   except Exception:
     cloudlog.exception("is_on_wifi failed")
     return False
