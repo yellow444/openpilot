6cf7604d0612a511d163d0ed836533264167d22d
mike <mike@mikesantifort.nl>
2020-11-05 17:52:23 +0100

    Tesla radar interface and calibration tools.

--- selfdrive/car/volkswagen/radar_tools/calibrateRadar.py
+++ selfdrive/car/volkswagen/radar_tools/calibrateRadar.py
@@ -10,10 +10,10 @@ import cereal.messaging as messaging
 from selfdrive.car.volkswagen.radar_interface import RadarInterface
 
 #for calibration we only want fixed objects within 1 m of the center line and between 2.5 and 4.5 m far from radar
-MINX = 2.5 
+MINX = 0.1
 MAXX = 14.5
-MINY = -1.0
-MAXY = 1.0
+MINY = -4.0
+MAXY = 4.0
 
 def get_rrext_by_trackId(rrext,trackId):
   if rrext is not None:
