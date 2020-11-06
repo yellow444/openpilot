4447650a0c0f0cd54fb32dea66cdc47ef60e9a74
mike <mike@mikesantifort.nl>
2020-11-06 16:55:29 +0100

    Tuning long/lat

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -109,7 +109,7 @@ class CarInterface(CarInterfaceBase):
       # PQ lateral tuning HCA_Status 7
       ret.lateralTuning.pid.kpBP = [0., 8., 35.]
       ret.lateralTuning.pid.kiBP = [0., 8., 35.]
-      ret.lateralTuning.pid.kpV = [0.15, 0.19, 0.21]
+      ret.lateralTuning.pid.kpV = [0.12, 0.17, 0.19]
       ret.lateralTuning.pid.kiV = [0.09, 0.10, 0.11]
 
       ret.stoppingControl = True
