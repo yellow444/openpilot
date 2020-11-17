4ec69c5efdc0f62723f29a82ad41ca79b06021e4
mike <mike@mikesantifort.nl>
2020-11-17 21:22:17 +0100

    Add ABS check on engagement, add StoppingWish to brake till 8km/h

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -93,9 +93,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.deadzoneBP = [0.]
       ret.longitudinalTuning.deadzoneV = [0.]
       ret.longitudinalTuning.kpBP = [0.]
-      ret.longitudinalTuning.kpV = [1.05]
+      ret.longitudinalTuning.kpV = [1.15]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [0.24]
+      ret.longitudinalTuning.kiV = [0.28]
 
       # PQ lateral tuning HCA_Status 7
       ret.lateralTuning.pid.kpBP = [0., 14., 35.]
