99836314118f7caeaa22d6f40c196cff2561d79e
mike <mike@mikesantifort.nl>
2020-11-18 20:33:47 +0100

    Tuning Long

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -93,9 +93,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.deadzoneBP = [0.]
       ret.longitudinalTuning.deadzoneV = [0.]
       ret.longitudinalTuning.kpBP = [0.]
-      ret.longitudinalTuning.kpV = [1.5]
+      ret.longitudinalTuning.kpV = [0.95]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [1.]
+      ret.longitudinalTuning.kiV = [0.12]
 
       # PQ lateral tuning HCA_Status 7
       ret.lateralTuning.pid.kpBP = [0., 14., 35.]
