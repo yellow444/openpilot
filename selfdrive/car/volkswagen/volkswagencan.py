22f4cdd612c630e5a26eec1b624d78bda7c049d5
mike <mike@mikesantifort.nl>
2021-08-31 18:21:33 +0200

    fix long

--- selfdrive/car/volkswagen/volkswagencan.py
+++ selfdrive/car/volkswagen/volkswagencan.py
@@ -118,7 +118,6 @@ def create_pq_pedal_control(packer, bus, apply_gas, idx):
   }
 
   if enable:
-    apply_gas = apply_gas * 1125.
     if (apply_gas < 227):
       apply_gas = 227
     values["GAS_COMMAND"] = apply_gas
