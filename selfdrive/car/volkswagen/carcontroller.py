6599c41b6b1f2c585ac26ef43be1c9f71fd78180
Edgy <mikesantifort@gmail.com>
2022-01-07 22:52:54 +0100

    Update carcontroller.py

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -124,7 +124,7 @@ class CarController():
           mobEnabled = False
 
         if CS.Stillstand:
-          self.haltenCounter + 1
+          self.haltenCounter = self.haltenCounter + 1
 
           if self.haltenCounter > 10:
             apply_brake = 0
