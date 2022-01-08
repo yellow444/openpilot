465f7450b2c09fb35c5b785528553039fab39fef
mike <mike@mikesantifort.nl>
2022-01-08 22:39:20 +0100

    fix controls

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -625,7 +625,7 @@ class Controls:
 
     if not self.read_only and self.initialized:
       # send car controls over can
-      self.last_actuators, can_sends = self.CI.apply(CC)
+      can_sends = self.CI.apply(CC)
       self.pm.send('sendcan', can_list_to_can_capnp(can_sends, msgtype='sendcan', valid=CS.canValid))
       CC.actuatorsOutput = self.last_actuators
 
